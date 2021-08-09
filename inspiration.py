1784444a32e81e668700955593396bea0cc2e910
lawrence501 <22851435+lawrence501@users.noreply.github.com>
2021-08-09 16:15:19 +1000

    Content

--- inspiration.py
+++ inspiration.py
@@ -1,6 +1,6 @@
 import random
 
-inspirationList = ["Hades", "Tem Tem", "Vampire Princess", "Doctor Who", "Witcher", "TWD", "Crumbling Keep", "Hannibal", "Buffy", "Angel", "Numenera", "ToME", "MTG", "D3", "PoE", "GW2", "DD", "D2", "UA", "GD", "Talisman",
+inspirationList = ["Black Tapes", "Hades", "Tem Tem", "Vampire Princess", "Doctor Who", "Witcher", "TWD", "Crumbling Keep", "Hannibal", "Buffy", "Angel", "Numenera", "ToME", "MTG", "D3", "PoE", "GW2", "DD", "D2", "UA", "GD", "Talisman",
                    "ES", "NW", "Median", "DOS", "D&D", "D&D", "LoZ", "DS", "BoI", "Pillars", "NA", "FO", "MD", "HoF", "EH", "FEH", "TL2", "Tyranny", "STS", "Pokemon", "AQ", "WoW", "DF", "BB", "E7", "LE", "PF", "WH"]
 
 if __name__ == "__main__":
