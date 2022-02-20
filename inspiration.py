647d5d9e6101883256ef74010d0a34574ffaa0b0
lawrence501 <22851435+lawrence501@users.noreply.github.com>
2022-02-20 13:00:38 +1100

    Beteen sessions

--- inspiration.py
+++ inspiration.py
@@ -1,6 +1,6 @@
 import random
 
-inspirationList = ["Fairytales", "John Cleaver", "LevelUp", "Gumshoe", "No Sleep", "New World", "Hades", "Tem Tem", "Vampire Princess", "Doctor Who", "Witcher", "TWD", "Hannibal", "Buffy", "Angel", "Numenera", "ToME", "MTG", "D3", "PoE", "GW2", "DD", "D2", "UA", "GD", "Talisman",
+inspirationList = ["Lost Ark", "Fairytales", "John Cleaver", "LevelUp", "Gumshoe", "No Sleep", "New World", "Hades", "Tem Tem", "Vampire Princess", "Doctor Who", "Witcher", "TWD", "Hannibal", "Buffy", "Angel", "Numenera", "ToME", "MTG", "D3", "PoE", "GW2", "DD", "D2", "UA", "GD", "Talisman",
                    "ES", "Neverwinter", "Median", "DOS", "D&D", "D&D", "LoZ", "DS", "BoI", "Pillars", "Nier", "FO", "MD", "HoF", "EH", "FEH", "TL2", "Tyranny", "STS", "Pokemon", "AQ", "WoW", "DF", "BB", "E7", "LE", "PF", "WH"]
 
 if __name__ == "__main__":
