3521eb9e9b671c16bc11f64418d44ffb3ed0f776
lawrence501 <22851435+lawrence501@users.noreply.github.com>
2022-05-17 21:21:41 +1000

    Session 12 dump

--- cmd/5e-cli/util.go
+++ cmd/5e-cli/util.go
@@ -259,7 +259,7 @@ func fetchMundanes(t string) ([]Mundane, error) {
 }
 
 func generateEncounter(location string) (string, error) {
-	chestChance := 0
+	chestChance := 5
 	positiveEncounterChance := 20 + chestChance
 	positiveRoll := rand.Intn(100)
 	if positiveRoll < chestChance {
