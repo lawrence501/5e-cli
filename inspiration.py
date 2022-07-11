22cd8dd3881a77516f449fafa9af75e64a982767
lawrence501 <22851435+lawrence501@users.noreply.github.com>
2022-07-11 15:48:44 +1000

    Pre-session

--- inspiration.py
+++ inspiration.py
@@ -1,6 +1,6 @@
 import random
 
-inspirationList = ["Diablo Immortal", "FFXIV", "Lost Ark", "Fairytales", "John Cleaver", "LevelUp", "Gumshoe", "No Sleep", "New World", "Hades", "Tem Tem", "Vampire Princess", "Doctor Who", "Witcher", "TWD", "Hannibal", "Buffy", "Angel", "Numenera", "ToME", "MTG", "D3", "PoE", "GW2", "DD", "D2", "UA", "GD", "Talisman",
+inspirationList = ["Marvel", "Diablo Immortal", "FFXIV", "Lost Ark", "Fairytales", "John Cleaver", "LevelUp", "No Sleep", "New World", "Hades", "Tem Tem", "Vampire Princess", "Doctor Who", "Witcher", "TWD", "Hannibal", "Buffy", "Angel", "Numenera", "ToME", "MTG", "D3", "PoE", "GW2", "DD", "D2", "UA", "GD", "Talisman",
                    "ES", "Neverwinter", "Median", "DOS", "D&D", "D&D", "LoZ", "DS", "BoI", "Pillars", "Nier", "FO", "MD", "HoF", "EH", "FEH", "TL2", "Tyranny", "STS", "Pokemon", "AQ", "WoW", "DF", "BB", "E7", "LE", "PF", "WH"]
 
 if __name__ == "__main__":
